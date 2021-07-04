4ec251a0e6e6108e139453d39220ab3e53333282
falkowa42 <falkowa42@gmail.com>
2021-07-04 13:36:05 -0400

    added in properties to compile and run in java 11

--- src/main/java/com/example/demo/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/demo/student/StudentConfig.java
@@ -1,22 +1,22 @@
-package com.example.demo.demo.student;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-@Configuration
-public class StudentConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
-        return args -> {
-            Student adam = new Student(1L, "Adam", LocalDate.of(2000, Month.JANUARY, 1), "test@gmail.com");
-            Student bob = new Student("Bob", LocalDate.of(2000, Month.JANUARY, 1), "test2@gmail.com");
-            studentRepository.saveAll(List.of(adam,bob));
-        };
-    }
-
-}
+//package com.example.demo.demo.student;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
+//
+//@Configuration
+//public class StudentConfig {
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+//        return args -> {
+//            Student adam = new Student(1L, "Adam", LocalDate.of(2000, Month.JANUARY, 1), "test@gmail.com");
+//            Student bob = new Student("Bob", LocalDate.of(2000, Month.JANUARY, 1), "test2@gmail.com");
+//            studentRepository.saveAll(List.of(adam,bob));
+//        };
+//    }
+//
+//}
